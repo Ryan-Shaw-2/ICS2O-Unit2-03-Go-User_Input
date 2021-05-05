1945002b65e11783f31a622a1ba784fa1e69e55b
RyanShaw21 <>
2021-05-05 17:28:31 +0000

    Created userInput.go

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ICS2O-Unit2-03-Go-User_Input
+[![GitHub's Super Linter](https://github.com/Ryan-Shaw-2/ICS2O-Unit2-03-Go-User_Input/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Ryan-Shaw-2/ICS2O-Unit2-03-Go-User_Input/actions)
---
+++ userInput.go
@@ -0,0 +1,25 @@
+// Created by: Ryan-Shaw-2
+// Created on: May 2021
+//
+// This program takes the users street number and name and shows the users address
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function takes the users street number and name and shows the users address
+	var streetNumber int
+	var streetName string
+
+	// input
+	fmt.Println("This program gets a users address.")
+	fmt.Println()
+	fmt.Print("Enter your street number: ")
+	fmt.Scanln(&streetNumber)
+	fmt.Print("Enter your street name: ")
+	fmt.Scanln(&streetName)
+
+	//ouput
+	fmt.Println("Your address is", streetNumber, streetName, ".")
+}
